73930e851f8f42d9a0f9090635ea6e4689f168ce
nicolapcweek94 <citizenwasp@gmail.com>
2013-10-11 13:34:47 +0200

    Added sorting options for -l

--- src/secret-octo-bear.py
+++ src/secret-octo-bear.py
@@ -4,7 +4,7 @@ import argparse
 import sqlite3
 import datetime
 
-conn = sqlite3.connect('expenses.db')
+conn = sqlite3.connect('/home/wasp/utils/expenses.db')
 c = conn.cursor()
 parser = argparse.ArgumentParser(description='Personal expenses manager.')
 
@@ -14,7 +14,11 @@ parser.add_argument("-p", "--price", dest="price", default="", help="Specifies t
 
 parser.add_argument("-d", "--date", dest="date", default="", help="Specifies the date of the expense. Optional.", metavar="YYYY-MM-DD")
 
-parser.add_argument("-l", "--list", dest="list", action="store_true", default="False", help="Lists expenses in the database.")
+parser.add_argument("-l", "--list", dest="lst", action="store_true", default="False", help="Lists expenses in the database.")
+
+parser.add_argument("-o", "--order", dest="orderby", action="store", default="date", help="Orders the list according to one field of the table. Possible values: date, price, desc. The default is date.", metavar="date")
+
+parser.add_argument("-t", "--type", dest="ordertype", default="asc", help="Specifies if the order is ascendent (asc) or descendent (desc). The default is asc.", metavar="asc")
 
 options = parser.parse_args()
 
@@ -22,8 +26,8 @@ if options.add != "":
   c.execute('''insert into expenses(date, desc, price) values (?, ?, ?)''', (datetime.date.today() if options.date == "" else options.date, options.add, float(options.price)))
   conn.commit()
 
-if options.list == True:
-  c.execute('''select * from expenses order by date''')
+if options.lst == True:
+  c.execute("select * from expenses order by " + options.orderby + " " + options.ordertype)
   rows = c.fetchall()
   for row in rows:
     print(row)
