08f48062a69c698c0ddd15bce7cb480c73990fab
nicolapcweek94 <citizenwasp@gmail.com>
2013-11-02 23:50:44 +0100

    Added the --remove option

--- src/sob.py
+++ src/sob.py
@@ -4,11 +4,13 @@ import argparse
 import sqlite3
 import datetime
 
-conn = sqlite3.connect('/home/wasp/utils/expenses.db')
+conn = sqlite3.connect('./expenses.db')
 c = conn.cursor()
 parser = argparse.ArgumentParser(description='Personal expenses manager.')
 
 parser.add_argument("-a", "--add", dest="add", default="", help="Adds a voice to the database.", metavar="DESCRIPTION")
+
+parser.add_argument("-r", "--remove", dest="remove", default="", help="Removes a voice from the database. The date of the operation must be specified too (with -d).", metavar="DESCRIPTION")
 
 parser.add_argument("-p", "--price", dest="price", default="", help="Specifies the amount of the expense.", metavar="PRICE")
 
@@ -21,6 +23,10 @@ parser.add_argument("-o", "--order", dest="orderby", action="store", default="da
 parser.add_argument("-t", "--type", dest="ordertype", default="asc", help="Specifies if the order is ascendent (asc) or descendent (desc). The default is asc.", metavar="asc")
 
 options = parser.parse_args()
+
+if options.remove != "" and options.date != "":
+    c.execute('''delete from expenses where date like ? and desc like ?''', (options.date, options.remove))
+    conn.commit()
 
 if options.add != "":
   c.execute('''insert into expenses(date, desc, price) values (?, ?, ?)''', (datetime.date.today() if options.date == "" else options.date, options.add, float(options.price)))
